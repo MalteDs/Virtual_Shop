ef5ecbc052f200249a8962c077933814ec38d4e4
lolito996 <josealejandromc4@gmail.com>
2023-04-26 14:33:30 -0500

    Upload Gson requierment

--- src/model/VirtualShop.java
+++ src/model/VirtualShop.java
@@ -1,20 +1,40 @@
 package model;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import com.google.gson.Gson;
+
 
 public class VirtualShop {
     private ArrayList<Orders> orders;
     private ArrayList<Product> products;
     private ArrayList<Product> orderProducts;
 
+    Gson gson;
+    File resultProducts;
+    File resultOrders;
+
     public VirtualShop() {
         orders = new ArrayList<>();
         orderProducts = new ArrayList<>();
         products = new ArrayList<>();
+        gson = new Gson();
+        File projectDir = new File(System.getProperty("user.dir"));
+        File dataDirectory = new File(projectDir.getAbsolutePath() + "/data");
+        if (!dataDirectory.exists()) {
+            dataDirectory.mkdir();
+        }
+        resultProducts = new File(dataDirectory, "resultProducts.json");
+        resultOrders = new File(dataDirectory, "resultOrders.json");
+
     }
 
-    public void addProductToInventory(String name, String description, int price, int amount, int purchasedNumber, int productCategory) throws Exception {
+
+    public String addProductToInventory(String name, String description, int price, int amount, int purchasedNumber, int productCategory) throws Exception {
         //Agregar productos al inventario
+        String msj="";
         for(Product product : products){
             if(product.getName().equalsIgnoreCase(name)){
                 throw new Exception("The product already exists in the inventory.");
@@ -22,19 +42,33 @@ public class VirtualShop {
         }
         Product newProduct = new Product(name, description, price, amount, purchasedNumber, productCategory);
         products.add(newProduct);
-        System.out.println("Product added successfully.");
+        converToGson(newProduct);
+        msj="Product added successfully.";
+        return msj;
+    }
+    public void converToGson(Product product){
+        String json = gson.toJson(product);
+        try (FileWriter fw = new FileWriter(resultProducts, true)) {
+            fw.write(json);
+        } catch (IOException e) {
+            System.out.println("Error al escribir en el archivo result.json: " + e.getMessage());
+        }
     }
 
-    public void deleteProductFromInventory(String nameProduct) {
+
+    public String deleteProductFromInventory(String nameProduct) {
+        String msj="";
         //Eliminar del inventario
         for (Product p : products) {
             if (p.getName().equals(nameProduct)) {
                 products.remove(p);
-//                System.out.println("Producto eliminado exitosamente.");
-                return;
+                msj="Producto eliminado exitosamente.";
+                }
             }
+        if(msj==null){
+            msj="Producto no encontrado en el inventario.";
         }
-    //        System.out.println("Producto no encontrado en el inventario.");
+        return msj;
     }
 
     public Product productExists(String nameProduct){
@@ -110,5 +144,22 @@ public class VirtualShop {
 //        System.out.println("Pedido no encontrado");
         return null;
     }
+    public boolean validateData(String name, String description, double price, int amount, int purchasedNumber, int productCategory){
+        boolean correct=true;
+        try{
+            if(name==null || description ==null || price <0 || amount<0 || purchasedNumber<0 || productCategory<0 || productCategory>8){
+                correct=false;
+            }else{
+                correct=true;
+            }
+        }catch(NumberFormatException e){
+            System.out.println("Hubo un error");
+
+        }catch(RuntimeException e){
+            System.out.println("Ocurrio un error");
+        }
+        return correct;
+    }
+
 
 }
